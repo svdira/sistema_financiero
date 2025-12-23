e03e2c83140ac477db5a311b9e387955abf3f58e
dsdira <svdira@outlook.com>
2025-12-23 17:01:28 -0600

    analisis_sistemafinanciero
    Primeras vistas del sistema financiero

--- raw_ssf/sfsv/views.py
+++ raw_ssf/sfsv/views.py
@@ -1,3 +1,108 @@
+
+from django.db import connection
 from django.shortcuts import render
 
-# Create your views here.
+def homepage(request):
+
+	tipo = request.GET.get('tipo', 'todos')
+
+	if tipo == 'todos':
+		sql = """
+			SELECT
+				Entidad,
+				Tipo,
+				round(sum(Saldo)/1000,1) as Saldo
+			FROM
+				carteras_clean
+			WHERE
+				mes = (select max(mes) from carteras_clean)
+			GROUP BY
+				Entidad,
+				Tipo
+			ORDER BY
+				round(sum(Saldo)/1000,1) desc
+		"""
+	else:
+		sql = f"""
+			SELECT
+				Entidad,
+				Tipo,
+				round(sum(Saldo)/1000,1) as Saldo
+			FROM
+				carteras_clean
+			WHERE
+				mes = (select max(mes) from carteras_clean)
+				and Tipo = '{tipo}'
+			GROUP BY
+				Entidad,
+				Tipo
+			ORDER BY
+				round(sum(Saldo)/1000,1) desc
+		"""
+
+
+	with connection.cursor() as cursor:
+		cursor.execute(sql)
+		rows = cursor.fetchall()
+	
+	labels = [row[0] for row in rows]
+	values = [row[2] for row in rows]
+
+	return render(request,'inicio.html',{'labels':labels,'values':values})
+
+
+def crecimiento(request):
+
+	institucion = request.GET.get('institucion', 'todos')
+
+	str_q = "WHERE 1 = 1" if institucion == 'todos' else f"WHERE Entidad = '{institucion}'"
+
+
+	sql = f"""
+		WITH base AS (
+		    SELECT
+		        mes,
+		        SUM(saldo) AS saldo
+		    FROM carteras_clean
+		    {str_q}
+		    GROUP BY mes
+		),
+		calc AS (
+		    SELECT
+		        mes,
+		        saldo,
+		        LAG(saldo) OVER (ORDER BY mes) AS saldo_anterior,
+		        FIRST_VALUE(saldo) OVER (
+		            ORDER BY mes
+		            ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+		        ) AS saldo_inicial
+		    FROM base
+		)
+		SELECT
+		    mes,
+		    saldo,
+		    saldo_anterior,
+		    ROUND(
+		        (saldo - saldo_anterior) * 100.0 / saldo_anterior,
+		        2
+		    ) AS variacion_pct_mes,
+		    ROUND(
+		        (saldo - saldo_inicial) * 100.0 / saldo_inicial,
+		        2
+		    ) AS crecimiento_pct_acumulado
+		FROM calc
+		WHERE mes > '2025-01-31'
+		ORDER BY mes;
+	"""
+
+
+
+	with connection.cursor() as cursor:
+		cursor.execute(sql)
+		rows = cursor.fetchall()
+	
+	labels = [row[0] for row in rows]
+	values = [row[3] for row in rows]
+	values_acc = [row[4] for row in rows]
+
+	return render(request,'crecimiento.html',{'labels':labels,'values':values,'values_acc':values_acc})
--- raw_ssf/svprestamos/settings.py
+++ raw_ssf/svprestamos/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sfsv'
 ]
 
 MIDDLEWARE = [
@@ -101,23 +103,38 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
+# https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/El_Salvador'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
 
+STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+
+
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
